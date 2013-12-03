97a58d2e29d322778dac3c6c147ac335cf579b7c
vagrant <vagrant@vagrant.(none)>
2013-12-03 10:20:04 +0100

    add undone controller view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 MyToDo::Application.routes.draw do
-  resources :tasks
+  resources :tasks do
+    collection do
+      get 'undone'
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
